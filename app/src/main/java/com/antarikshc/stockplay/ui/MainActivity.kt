754e26105f43e1d664f4cb29d2feaca46bde59c6
Antariksh Chavan <antarikshc@gmail.com>
2020-09-04 23:14:08 +0530

    Initial codebase
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

---
+++ app/src/main/java/com/antarikshc/stockplay/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.antarikshc.stockplay.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.antarikshc.stockplay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

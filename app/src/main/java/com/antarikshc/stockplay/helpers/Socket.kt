be867252f1673d330b61b9ed5244092d57e88d52
Antariksh Chavan <antarikshc@gmail.com>
2020-09-06 05:08:34 +0530

    feat: Add Network retry for WebSocket
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/stockplay/helpers/Socket.kt
+++ app/src/main/java/com/antarikshc/stockplay/helpers/Socket.kt
@@ -2,7 +2,9 @@ package com.antarikshc.stockplay.helpers
 
 import android.util.Log
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.retryWhen
 import okhttp3.*
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -34,12 +36,30 @@ class Socket @Inject constructor(private val client: OkHttpClient) {
             }
 
             override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-                Log.d(TAG, "Closed")
+                if (code != 1000) close(SocketNetworkException("Network Failure"))
+                Log.d(TAG, "Closed #$code")
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                close(SocketNetworkException("Network Failure"))
+                Log.d(TAG, "Network Failure")
             }
         })
 
         // Wait for the Flow to finish
         awaitClose { webSocket.close(1000, "Closed") }
     }
+        .retryWhen { cause, attempt ->
+            // Exponential backoff of 1 second on each retry
+            if (attempt > 1) delay(1000 * attempt)
+            else if (attempt >= 8) delay(8000)
+
+            Log.d(TAG, "Retrying #$attempt")
+
+            // Do not retry for IllegalArgument or 3 attempts are reached
+            cause is SocketNetworkException
+        }
+
+    class SocketNetworkException(message: String) : Exception(message)
 
 }
